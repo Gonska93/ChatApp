c0f878666edaaf5ee4ad41656072123ebc3f7434
Gonska93 <gonska9933@gmail.com>
2019-08-13 19:36:52 +0500

    Add Contracts folder, with Requests and Responses structure, Added Registration request and response classes

---
+++ Backend/ChatApp/Contracts/Responses/UserRegistrationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Contracts.Responses
+{
+    public class UserRegistrationResponse
+    {
+        public bool Ok { get; set; }
+        public string Description { get; set; }
+    }
+}
