363c94ab176283f9da894abdfa7585d5fddc1922
Gonska93 <gonska9933@gmail.com>
2019-08-13 16:26:20 +0500

    Add OnConnected and OnDisconnected handling which will send 'Counter' signal to all connected clients

--- Backend/ChatApp/Hubs/ChatHub.cs
+++ Backend/ChatApp/Hubs/ChatHub.cs
@@ -9,9 +9,24 @@ namespace ChatApp.Hubs
 {
     public class ChatHub : Hub
     {
+        public static HashSet<string> ConnectionIds = new HashSet<string>();
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            ConnectionIds.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("Counter", ConnectionIds.Count);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            ConnectionIds.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("Counter", ConnectionIds.Count);
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
