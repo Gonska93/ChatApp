1b0314919a7e731fbea390f07fc9b2fc6cb16856
Gonska93 <gonska9933@gmail.com>
2019-08-13 14:15:24 +0500

    Change chat graphical interface to look as required

--- Frontend/public/main.js
+++ Frontend/public/main.js
@@ -1,7 +1,7 @@
 // /public/javascript.js
 
 // Get the current username from the cookies
-var user = cookie.get('user');
+var user = getNameFromCookie();
 if (!user) {
 
   // Ask for the username if there is none set already
@@ -10,6 +10,39 @@ if (!user) {
     alert('We cannot work with you like that!');
   } else {
     // Store it in the cookies for future use
-    cookie.set('user', user);
+    setNameInCookie(user);
   }
+}
+
+function getNameFromCookie() {
+  
+	var name = "user=";
+  
+	var ca = document.cookie.split(';');
+  
+	for(var i = 0; i < ca.length; i++) {
+    
+		var c = ca[i];
+    
+		while (c.charAt(0) == ' ') {
+	
+      		c = c.substring(1);
+    }
+
+	      		if (c.indexOf(name) == 0) {
+		
+      		return c.substring(name.length, c.length);
+    
+		      		}	
+  
+	      		}
+
+			
+return "";
+
+	}
+
+
+function setNameInCookie(name) {
+  document.cookie = "user=" + name;
 }
